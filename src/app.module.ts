7e8bcae5a4af1c56344de898f1064cc0d32c42cb
Rodrigo Revollo <rodrigorevollogutierrez@gmail.com>
2026-06-13 22:28:12 -0400

    conexion BD + Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.BD_HOST,
+      port: +`${process.env.BD_PORT}` || 5432,
+      username: process.env.BD_USERNAME,
+      password: process.env.BD_PASSWORD,
+      database: process.env.BD_DATABASE,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false, // Nunca usar synchronize: true en producción, ya que puede causar pérdida de datos
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
